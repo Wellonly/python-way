2d91aec7f84eb487143558495bf3384cace65b9a
Well <iconn8 at gmail dot com>
2023-02-18 16:05:37 +0000

    refactor lsjson()

--- os/lsjson.py
+++ os/lsjson.py
@@ -2,30 +2,27 @@
 import sys
 import json
 
-def printDictonary(level, branch, dic):
-  global ln, args
-  for k, v in dic.items():
-    if level <= 0 or k == args[ln-level]:
-      if type(v) == dict:
-        printDictonary(level-1, f'{branch}:{k}', v)
-      else:
-        print(f'{branch}:{k}:{v}')
-
 def lsjson(argv):
   '''..usage: lsjson <file> [branch [subbranch]]; print a content of file(.json) with entries on specified branch or all content'''
   if len(argv) == 1 or argv[1] == '--help':
     print(lsjson.__doc__)
     quit(4)
 
-  global ln, args
-  args = argv
-  ln   = len(args)
-  level= ln-2
+  def _printDictonary(level, branch, dic):
+    nonlocal argv
+    for k, v in dic.items():
+      if level <= 0 or k == argv[-level]:
+        if type(v) == dict:
+          _printDictonary(level-1, f'{branch}:{k}', v)
+        else:
+          print(f'{branch}:{k}:{v}')
+
+  level=len(argv)-2
   with open(argv[1]) as js:
     for k, v in json.load(js).items():
-      if level == 0 or k == args[ln-level]:
+      if level == 0 or k == argv[-level]:
         if type(v) == dict:
-          printDictonary(level-1, k, v)
+          _printDictonary(level-1, k, v)
         else:
           print(f'{k}:{v}')
 
